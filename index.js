b8cb72650e15a585484d62877d5b0e63790ef949
jkaddu <jkaddu@yahoo.com>
2021-07-31 14:31:12 -0700

    portfolio work transfered

---
+++ index.js
@@ -0,0 +1,59 @@
+// Home links
+const one = document.querySelector(".l1")
+
+one.addEventListener("mouseenter", function() {
+    one.style.transform = 'scale(1.2)'
+    one.style.transition = 'all 0.3s'
+})
+
+one.addEventListener("mouseleave", function() {
+    one.style.transform = 'scale(1)'
+})
+
+const two = document.querySelector(".l2")
+
+two.addEventListener("mouseenter", function() {
+    two.style.transform = 'scale(1.2)'
+    two.style.transition = 'all 0.3s'
+})
+
+two.addEventListener("mouseleave", function() {
+    two.style.transform = 'scale(1)'
+})
+
+
+const three = document.querySelector(".l3")
+
+three.addEventListener("mouseenter", function() {
+    three.style.transform = 'scale(1.2)'
+    three.style.transition = 'all 0.3s'
+})
+
+three.addEventListener("mouseleave", function() {
+    three.style.transform = 'scale(1)'
+})
+
+
+// Resume links
+
+const anchorOne = document.querySelector(".link")
+
+anchorOne.addEventListener("mouseenter", function() {
+    anchorOne.style.transform = 'scale(1.2)'
+    anchorOne.style.transition = 'all 0.3s'
+})
+
+anchorOne.addEventListener("mouseleave", function() {
+    anchorOne.style.transform = 'scale(1)'
+})
+
+const ancR1 = document.querySelector(".r1")
+
+ancR1.addEventListener("mouseenter", function() {
+    ancR1.style.transform = 'scale(1.2)'
+    ancR1.style.transition = 'all 0.3s'
+})
+
+ancR1.addEventListener("mouseleave", function() {
+    ancR1.style.transform = 'scale(1)'
+})
