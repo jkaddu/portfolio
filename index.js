7588f537f86e7e2e10e273c6ef259352366dc1d8
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Pro.local>
2022-12-16 15:22:18 -0800

    updated prjct sect btns

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const one = document.querySelector(".l1");
 const two = document.querySelector(".l2");
 const three = document.querySelector(".l3");
 const four = document.querySelector(".l4");
+const box = document.querySelector(".box_btn");
+const box2 = document.querySelector(".box_btn2");
+const box3 = document.querySelector(".box_btn3");
 
 one.addEventListener("click", function () {
   window.location.href = "#about";
@@ -54,4 +57,43 @@ four.addEventListener("mouseenter", function () {
 
 four.addEventListener("mouseleave", function () {
   four.style.transform = "scale(1)";
+});
+
+box.addEventListener("click", function () {
+  window.location.href = "https://bookr-jkaddu.vercel.app/";
+});
+
+box.addEventListener("mouseenter", function () {
+  box.style.transform = "scale(1.05)";
+  box.style.transition = "all 0.3s";
+});
+
+box.addEventListener("mouseleave", function () {
+  box.style.transform = "scale(1)";
+});
+
+box2.addEventListener("click", function () {
+  window.location.href = "https://create-react-app-jkaddu.vercel.app/";
+});
+
+box2.addEventListener("mouseenter", function () {
+  box2.style.transform = "scale(1.05)";
+  box2.style.transition = "all 0.3s";
+});
+
+box2.addEventListener("mouseleave", function () {
+  box2.style.transform = "scale(1)";
+});
+
+box3.addEventListener("click", function () {
+  window.location.href = "https://wcc2-g1gne3zmb-jkaddu.vercel.app/";
+});
+
+box3.addEventListener("mouseenter", function () {
+  box3.style.transform = "scale(1.05)";
+  box3.style.transition = "all 0.3s";
+});
+
+box3.addEventListener("mouseleave", function () {
+  box3.style.transform = "scale(1)";
 });
