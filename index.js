03963ac548941ad9f76ed2a29caa5d90993886e2
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-01-01 10:43:20 -0800

    smooth scroll implemented for projects sec

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ for (let i = 0; i < navBtn.length; i++) {
 
 // Tags
 const aboutBtn = document.querySelector(".scroll-about");
+const projectsBtn = document.querySelector(".scroll-projects");
 const contactBtn = document.querySelector(".scroll-contact");
 const aboutSection = document.querySelector(".about_section");
+const projectsSection = document.querySelector(".projects_section");
 const contactFooter = document.querySelector("footer");
 
 aboutBtn.addEventListener("click", function () {
   aboutSection.scrollIntoView({ behavior: "smooth" });
+});
+projectsBtn.addEventListener("click", function () {
+  projectsSection.scrollIntoView({ behavior: "smooth" });
 });
 contactBtn.addEventListener("click", function () {
   contactFooter.scrollIntoView({ behavior: "smooth" });
@@ -40,27 +45,4 @@ two.addEventListener("click", function () {
 
 three.addEventListener("click", function () {
   window.location.href = "https://wcc2-g1gne3zmb-jkaddu.vercel.app/";
-});
-
-// Footer Links
-const tags = document.querySelector(".tags");
-
-// for (let i = 0; i < tags.length; i++) {
-//   tags[i].addEventListener("mouseenter", function () {
-//     tags[i].style.transform = "scale(1.05)";
-//     tags[i].style.transition = "all 0.3s";
-//   });
-
-//   tags[i].addEventListener("mouseleave", function () {
-//     tags[i].style.transform = "scale(1)";
-//   });
-// }
-
-tags.addEventListener("mouseenter", function () {
-  tags.style.transform = "scale(1.1)";
-  tags.style.transition = "all 0.3s";
-});
-
-tags.addEventListener("mouseleave", function () {
-  tags.style.transform = "scale(1)";
 });
